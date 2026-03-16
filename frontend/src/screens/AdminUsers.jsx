73bcc5909b094e114b40c0be677ee9aa2ee8c8fc
Benj Patrick <benj@example.com>
2026-03-16 19:59:42 +0800

    Fix unused imports in AdminUsers and SellerDashboard

--- frontend/src/screens/AdminUsers.jsx
+++ frontend/src/screens/AdminUsers.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../axiosInstance';
 import './AdminUsers.css';
